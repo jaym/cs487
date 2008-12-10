5a071843528b152b31e584629f845155f07e4e8a
Jay Mundrawala <jmundraw@iit.edu>
2008-12-09 18:12:41 -0600

    I think its done

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -1,4 +1,13 @@
 class Requirement < ActiveRecord::Base
      belongs_to :project, :foreign_key => 'project_id'
-   has_many :test_plans
+     has_many :test_plans
+     validates_presence_of :name, :description
+     validates_format_of :status, :with => /(Accepted|Rejected|Holding|Analysis|Review|Needs Review|Reviewed)/, :message => "Invalid Status"
+     validate
+     protected
+     def validate
+       if((status == 'Accepted' || status == 'Reviewed' || status == 'Review') && test_plans.size == 0)
+         errors.add("Must submit test plan!")
+       end
+     end
 end
--- app/models/test_plan.rb
+++ app/models/test_plan.rb
@@ -1,2 +1,6 @@
 class TestPlan < ActiveRecord::Base
+  belongs_to :requirement
+  validates_presence_of :description
+
+
 end
